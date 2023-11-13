201dfd6b4768e8bf8ec8cbf5631759e3cd3a54df
Pasik <parasatto@gmail.com>
2023-11-14 00:24:04 +0600

    hash_tables

---
+++ data_structures/hash_tables.py
@@ -0,0 +1,12 @@
+# Создание таблицы с уникальными номерами и списками имени и фамилии
+table = {
+    1: ['John', 'Doe'],
+    2: ['Jane', 'Smith'],
+    3: ['Alice', 'Johnson'],
+    4: ['Bob', 'Williams'],
+    # Добавьте другие записи по мере необходимости
+}
+
+# Вывод таблицы
+for key, value in table.items():
+    print(f"Key: {key}, Value: {value}")
