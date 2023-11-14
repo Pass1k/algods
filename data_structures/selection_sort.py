b758f13df3efc27bf86031fc69388d64230e9b20
Pasik <parasatto@gmail.com>
2023-11-14 15:10:47 +0600

    selection_sort and mega_sort

---
+++ data_structures/selection_sort.py
@@ -0,0 +1,18 @@
+array = [9, 7, 4, 1, -1, -2, 3]
+
+lens = len(array)
+
+for i in range(lens - 1):
+    m = array[i]
+    p = i
+    for j in range(i+1, lens):
+        if m > array[j]:
+            m = array[j]
+            p = j
+        
+    if p != i:
+        t = array[i]
+        array[i] = array[p]
+        array[p] = t
+
+print(array)
