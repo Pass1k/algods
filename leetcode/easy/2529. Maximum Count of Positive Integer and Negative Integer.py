4eed1f2afb559bb6f99d0640bf70588ebf6ab008
Pasik <parasatto@gmail.com>
2023-11-23 13:12:53 +0600

    Dijkstra`s algorithms

---
+++ leetcode/easy/2529. Maximum Count of Positive Integer and Negative Integer.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maximumCount(self, nums):
+        return max(
+            len([x for x in nums if x < 0]),
+            len([x for x in nums if x > 0])
+        )
+        
